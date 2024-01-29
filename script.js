1b4c8d8f322bc371084ca2693ed708c259d3d7ae
Quezty <storlijoachim@gmail.com>
2024-01-29 19:25:45 +0100

    problem med getJSON

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
-  console.log('hello world'); 
+  let jsonData;
   $.getJSON('users.json', function(data) {
     jsonData = data;
-    console.log(jsonData[0])
-  })
-    .done(function () {
-      console.log('JSON file');
+  });
+  console.log(jsonData[0])  
+  function validateForm() {
+    var usernameField = $("#usernameInput").val();
+    var passwordField = $("#passwordInput").val();   
+    $.getJSON('users.json', function(data) {
+      jsonData = data;
     });
+    console.log(jsonData[0])
+    // if(jsonData[0].username == usernameField && jsonData[0].password == passwordField) {
+    //   console.log("Yay!")
+    // } else {
+    //   alert("Feil brukernavn eller passord")
+    // }
+
+  }
+  
+  $("#loginForm").submit(function(event) {
+    event.preventDefault();
+    validateForm();
+  });
+
+
 });
