da694482c10a0fc45f05adb4a28ffcd6cb806c17
Quezty <storlijoachim@gmail.com>
2024-01-30 08:23:23 -0800

    login logic complete

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
-  let jsonData = {};
-  $.getJSON('users.json', function(data) {
-    validateForm(data)
-  });
+
+
 
   function validateForm(json) {
-    var usernameField = $("#usernameInput").val();
-    var passwordField = $("#passwordInput").val();   
+      var usernameField = $("#usernameInput").val();
+      var passwordField = $("#passwordInput").val();   
+      if (usernameField == json[0].username && passwordField == json[0].password) {
+        $("#usernameInput, #passwordInput").val("");
+        return true;
+      } else {
+        alert("feil brukernavn eller passord")
+        $("#usernameInput, #passwordInput").val("");
+        return false;
+      }
+  }
+
+  function showFormEditing() {
     
   }
-  
+
   $("#loginForm").submit(function(event) {
     event.preventDefault();
-    validateForm();
+    $.getJSON('users.json', function(data) {
+      if (validateForm(data) == true) {
+        showFormEditing()
+      } 
+    });
   });
+
+
 });
