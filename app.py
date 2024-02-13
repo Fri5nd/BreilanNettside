78f3e87feac003244742e41c98c1318d83f3c291
J-Kjellmo <joamath@outlook.com>
2024-02-13 15:08:16 +0100

    Fixed my braindamage, routed new pages. om_oss.html basically done, working contact page but not done

--- app.py
+++ app.py
@@ -126,13 +126,13 @@ def delete_tournament(tournament_id):
         db.session.commit()
     return redirect(url_for('crew_panel'))
 
-@app.route('/om_oss', methods=['GET'])
+@app.route('/om_oss', methods=['GET', 'POST'])
 def omOss():
-    render_template('om_oss.html')
+    return render_template('om_oss.html')
 
-@app.route('/kontakt_oss', methods=['GET'])
+@app.route('/kontakt_oss', methods=['GET', 'POST'])
 def kontaktOss():
-    render_template('kontakt.html')
+    return render_template('kontakt.html')
 
 # runs the app on the flask development server
 if __name__ == '__main__':
